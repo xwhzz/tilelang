85c157bf7b872511f43ba7edfec61ae32493780c
xwhzz <wh.xie@outlook.com>
2025-03-25 02:56:13 +0800

    update

---
+++ test.py
@@ -0,0 +1,12 @@
+from tabulate import tabulate
+
+
+table = [[
+    "original", 1, 1, 1, 1
+], [
+    "current", 1, 1, 1, 1
+]]
+
+headers = ["version", "Best Latency (s)", "Best TFlops", "Reference TFlops", "Best Config"]
+
+print(tabulate(table, headers=headers, tablefmt="github", stralign="left", numalign="decimal"))
