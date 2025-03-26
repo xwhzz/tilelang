a365224a7c8242f22ff6fba7b90e599a44350ad7
xwhzz <wh.xie@outlook.com>
2025-03-27 01:18:26 +0800

    fix

--- maint/scripts/build_docs.sh
+++ maint/scripts/build_docs.sh
@@ -3,8 +3,14 @@
 # Copyright (c) Tile-AI Corporation.
 # Licensed under the MIT License.
 
-cd docs
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m pip install -r docs/requirements.txt
+python -m pip install .
 
+cd docs
 make html
 
 cp CNAME _build/html/
