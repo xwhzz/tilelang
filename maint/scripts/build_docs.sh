5501b31c3b896a85323f691f824475e8edeaddac
Wenhao Xie <wh.xie@outlook.com>
2025-03-27 17:32:19 +0800

    [Doc] Python API docs generation (#278)
    * fix bug
    
    * update performance.py
    
    * update python api docs
    
    * test workflow
    
    * fix dependency
    
    * fix bug
    
    * fix
    
    * update correct git config
    
    * test workflow
    
    * clear cache
    
    * lint fix
    
    * fix exclude path

--- maint/scripts/build_docs.sh
+++ maint/scripts/build_docs.sh
@@ -1,7 +1,11 @@
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements-test.txt
+python -m pip install -r docs/requirements.txt
+python -m pip install -e .
+
 cd docs
-
-pip install -r requirements.txt
-
 make html
 
 cp CNAME _build/html/
