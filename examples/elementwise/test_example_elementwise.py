dd7eb48885629d2ccdcaac399dfdcd4871f989e9
Cunxiao Ni <85601223+Cunxiao2002@users.noreply.github.com>
2025-05-09 10:32:03 +0800

    [CI] Add elementwise and gemv examples to CI. (#458)
    * [CI] Add elementwise and gemv examples to CI.
    
    * fix lint
    
    * test
    
    * fix gemv lint
    
    * fix lint

---
+++ examples/elementwise/test_example_elementwise.py
@@ -0,0 +1,10 @@
+import tilelang.testing
+import example_elementwise_add
+
+
+def test_example_elementwise_add():
+    example_elementwise_add.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
---
+++ examples/gemv/test_example_gemv.py
@@ -0,0 +1,11 @@
+import tilelang.testing
+
+import example_gemv
+
+
+def test_example_gemv():
+    example_gemv.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
