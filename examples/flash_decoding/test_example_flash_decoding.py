7b66fb19c2429a198cd6f52e74c12e359900e71d
徐畅 <147292722+xuchangtolearn@users.noreply.github.com>
2025-05-13 13:02:02 +0800

    [CI] Add flash_decoding example to CI (#487)
    * [CI] Add flash_decoding example to CI
    
    * Add output of ref latency
    
    * format example_gqa_decode.py

---
+++ examples/flash_decoding/test_example_flash_decoding.py
@@ -0,0 +1,16 @@
+import tilelang.testing
+
+import example_gqa_decode
+import example_mha_inference
+
+
+def test_example_example_gqa_decode():
+    example_gqa_decode.main()
+
+
+def test_example_example_mha_inference():
+    example_mha_inference.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
