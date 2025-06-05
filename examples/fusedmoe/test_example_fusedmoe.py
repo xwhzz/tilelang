88c622c9d74478e73ce8834f619e89af84698b78
Zhengju Tang <97930865+tzj-fxz@users.noreply.github.com>
2025-06-05 22:39:48 +0800

    [CI] Add FusedMoE example (#555)
    * [CI] Add FusedMoE example
    
    * Lint
    
    * Fix import bug
    
    * Fix comment bug
    
    * Update example_fusedmoe_torch.py
    
    ---------
    
    Co-authored-by: Lei Wang <34334180+LeiWang1999@users.noreply.github.com>

---
+++ examples/fusedmoe/test_example_fusedmoe.py
@@ -0,0 +1,10 @@
+import tilelang.testing
+import example_fusedmoe_tilelang
+
+
+def test_example_fusedmoe_tilelang():
+    example_fusedmoe_tilelang.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
