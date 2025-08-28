ea5483016d0bb4ea27c7b695ef3a9d66e44c493c
Zhengju Tang <97930865+tzj-fxz@users.noreply.github.com>
2025-08-29 02:00:58 +0800

    [MXFP4] Add 1D TMA copy for Scale tensor in MXFP4 GEMM (#766)
    * [TMA] Add 1D TMA copy for Scale tensor
    
    * [Lint]
    
    * [Test] Add test for kernel
    
    * [BugFix]

--- examples/dequantize_gemm/test_example_dequantize_gemm.py
+++ examples/dequantize_gemm/test_example_dequantize_gemm.py
@@ -3,6 +3,7 @@ import tilelang.testing
 import example_dequant_gemv_fp16xint4
 import example_dequant_gemm_fp4_hopper
 import example_dequant_gemm_bf16_mxfp4_hopper
+import example_dequant_gemm_bf16_mxfp4_hopper_tma
 
 
 @tilelang.testing.requires_cuda
@@ -20,6 +21,12 @@ def test_example_dequant_gemm_fp4_hopper():
 @tilelang.testing.requires_cuda_compute_version_ge(9, 0)
 def test_example_dequant_gemm_bf16_mxfp4_hopper():
     example_dequant_gemm_bf16_mxfp4_hopper.main()
+
+
+@tilelang.testing.requires_cuda
+@tilelang.testing.requires_cuda_compute_version_ge(9, 0)
+def test_example_dequant_gemm_bf16_mxfp4_hopper_tma():
+    example_dequant_gemm_bf16_mxfp4_hopper_tma.main()
 
 
 if __name__ == "__main__":
