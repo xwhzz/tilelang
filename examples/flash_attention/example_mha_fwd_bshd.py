907c3ff05b2ab637058a26bac841e878aa6ef177
botbw <wang1570@e.ntu.edu.sg>
2025-09-16 17:21:08 +0800

    [Example] Remove redundant param (#821)

--- examples/flash_attention/example_mha_fwd_bshd.py
+++ examples/flash_attention/example_mha_fwd_bshd.py
@@ -193,7 +193,7 @@ def main(
         print("Tile-lang: {:.2f} ms".format(latency))
         print("Tile-lang: {:.2f} TFlops".format(total_flops / latency * 1e-9))
     else:
-        best_result = flashattn(batch, heads, seq_len, dim, is_causal, tune=tune)
+        best_result = flashattn(batch, heads, seq_len, dim, is_causal)
         best_latency = best_result.latency
         best_config = best_result.config
         ref_latency = best_result.ref_latency
