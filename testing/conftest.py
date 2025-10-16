a79bc5c61d245e8866d021453408fe9881e786f2
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-10-16 20:38:23 +0800

    [CI] Fix ROCm CI (#1043)
    * [CI] fix ROCm CI
    
    * feat: add a hook to error out on no test runs

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,5 +1,6 @@
 import os
 import random
+import pytest
 
 os.environ["PYTHONHASHSEED"] = "0"
 
@@ -18,3 +19,26 @@ except ImportError:
     pass
 else:
     np.random.seed(0)
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    """Ensure that at least one test is collected. Error out if all tests are skipped."""
+    known_types = {
+        "failed",
+        "passed",
+        "skipped",
+        "deselected",
+        "xfailed",
+        "xpassed",
+        "warnings",
+        "error",
+    }
+    if (sum(
+            len(terminalreporter.stats.get(k, []))
+            for k in known_types.difference({"skipped", "deselected"})) == 0):
+        terminalreporter.write_sep(
+            "!",
+            (f"Error: No tests were collected. "
+             f"{dict(sorted((k, len(v)) for k, v in terminalreporter.stats.items()))}"),
+        )
+        pytest.exit("No tests were collected.", returncode=5)
