17bd0a6c651f599bec1397e0b91830c3ddc93076
Tong WU <109033598+Rachmanino@users.noreply.github.com>
2025-10-19 17:34:12 +0800

    [Enhancement] Deprecate split&sum in attn bwd examples on Hopper and migrate to vectorized atomic add (#1065)

--- examples/flash_attention/example_mha_bwd_wgmma_pipelined.py
+++ examples/flash_attention/example_mha_bwd_wgmma_pipelined.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn.functional as F
 import tilelang
-from tilelang.autotuner import *
 import tilelang.language as T
+from tilelang.profiler import do_bench
 import argparse
 
 
@@ -112,37 +112,6 @@ def flashattn_bwd_preprocess(batch, heads, seq_len, dim):
     return flash_bwd_prep
 
 
-def make_dq_layout(dQ):
-    # atomicAdd can not be vectorized, so we need to reorder dq to match the 8x8 gemm fragment
-    return T.Layout(dQ.shape,
-                    lambda b, l, h, d: [b, l // 8, h, d // 8, (d % 2), 4 * (l % 8) + (d % 8) // 2])
-
-
-@tilelang.jit(
-    out_idx=[1], pass_configs={
-        tilelang.PassConfigKey.TL_ENABLE_FAST_MATH: True,
-    })
-def flashattn_bwd_postprocess(batch, heads, seq_len, dim):
-    dtype = "float16"
-    accum_dtype = "float"
-    shape = [batch, seq_len, heads, dim]
-    blk = 64
-
-    @T.prim_func
-    def flash_bwd_post(
-            dQ: T.Tensor(shape, accum_dtype),  # type: ignore
-            dQ_out: T.Tensor(shape, dtype),  # type: ignore
-    ):
-        with T.Kernel(T.ceildiv(seq_len, blk), heads, batch, threads=128) as (bx, by, bz):
-            T.annotate_layout({dQ: make_dq_layout(dQ)})
-            T.copy(
-                dQ[bz, bx * blk:(bx + 1) * blk, by, :],
-                dQ_out[bz, bx * blk:(bx + 1) * blk, by, :],
-            )
-
-    return flash_bwd_post
-
-
 @tilelang.jit(pass_configs={
     tilelang.PassConfigKey.TL_ENABLE_FAST_MATH: True,
 })
@@ -186,12 +155,13 @@ def flashattn_bwd(batch, heads, seq_len, dim, is_causal, block_M, block_N):
             dq = T.alloc_fragment([block_N, dim], accum_dtype)
             dv_shared = T.alloc_shared([block_M, dim], dtype)
             dk_shared = T.alloc_shared([block_M, dim], dtype)
+            dq_shared = T.alloc_shared([block_N, dim], accum_dtype)
 
             T.annotate_layout({
-                dQ: make_dq_layout(dQ),
                 K_shared: tilelang.layout.make_swizzled_layout(K_shared),
                 dv_shared: tilelang.layout.make_swizzled_layout(dv_shared),
                 dk_shared: tilelang.layout.make_swizzled_layout(dk_shared),
+                dq_shared: tilelang.layout.make_swizzled_layout(dq_shared),
             })
 
             T.copy(K[bz, by * block_M:(by + 1) * block_M, bx, :], K_shared)
@@ -237,8 +207,8 @@ def flashattn_bwd(batch, heads, seq_len, dim, is_causal, block_M, block_N):
                 T.clear(dq)
                 T.gemm(dsT_shared, K_shared, dq, transpose_A=True, wg_wait=1)
                 T.wait_wgmma(0)
-                for i, j in T.Parallel(block_N, dim):
-                    T.atomic_add(dQ[bz, k * block_N + i, bx, j], dq[i, j])
+                T.copy(dq, dq_shared)
+                T.atomic_add(dQ[bz, k * block_N:(k + 1) * block_N, bx, :], dq_shared)
             T.copy(dv, dv_shared)
             T.copy(dk, dk_shared)
             T.copy(dv_shared, dV[bz, by * block_M:(by + 1) * block_M, bx, :])
@@ -274,7 +244,6 @@ class _attention(torch.autograd.Function):
         block_M = 128
         block_N = 128 if D_HEAD <= 64 else 32
         mod_prep = flashattn_bwd_preprocess(BATCH, H, N_CTX, D_HEAD)
-        mod_post = flashattn_bwd_postprocess(BATCH, H, N_CTX, D_HEAD)
         delta = mod_prep(o, do)
         mod = flashattn_bwd(BATCH, H, N_CTX, D_HEAD, ctx.causal, block_M, block_N)
         shape = [BATCH, N_CTX, H, D_HEAD]
@@ -282,7 +251,7 @@ class _attention(torch.autograd.Function):
         dk = torch.empty(shape, dtype=torch.float16, device=q.device)
         dv = torch.empty(shape, dtype=torch.float16, device=q.device)
         mod(q, k, v, do, lse, delta, dq, dk, dv)
-        dq = mod_post(dq)
+        dq = dq.to(torch.float16)
         return dq, dk, dv, None
 
 
@@ -336,14 +305,13 @@ def main(
     assert torch.allclose(dV, dV_ref, rtol=1e-2, atol=1e-2)
     assert torch.allclose(dK, dK_ref, rtol=1e-2, atol=1e-2)
     assert torch.allclose(dQ, dQ_ref, rtol=1e-2, atol=1e-2)
+    print('All checks passed.✅')
 
     def run():
         O_ref.backward(dO, retain_graph=True)
 
     def run1():
         O.backward(dO, retain_graph=True)
-
-    from tilelang.profiler import do_bench
 
     latency = do_bench(run, warmup=500)
     print("torch: {:.2f} ms".format(latency))
