ce60770fb0977f0f054b81297a7dba16161d883d
xwhzz <wh.xie@outlook.com>
2025-12-22 01:09:45 +0000

    upd

--- examples/attention_sink/regression_attention_sink.py
+++ examples/attention_sink/regression_attention_sink.py
@@ -12,7 +12,7 @@ def regression_example_mha_sink_fwd_bhsd():
 
 def regression_example_mha_sink_fwd_bhsd_sliding_window():
     tilelang.testing.process_func(
-        example_mha_sink_fwd_bhsd.run_regression_perf, window_size=128
+        example_mha_sink_fwd_bhsd.run_regression_perf, "regression_example_mha_sink_fwd_bhsd_sliding_window", window_size=128
     )
 
 
@@ -22,7 +22,7 @@ def regression_example_mha_sink_fwd_bhsd_wgmma_pipelined():
 
 def regression_example_mha_sink_fwd_bhsd_wgmma_pipelined_sliding_window():
     tilelang.testing.process_func(
-        example_mha_sink_fwd_bhsd_wgmma_pipelined.run_regression_perf, window_size=128
+        example_mha_sink_fwd_bhsd_wgmma_pipelined.run_regression_perf, "regression_example_mha_sink_fwd_bhsd_wgmma_pipelined_sliding_window", window_size=128
     )
 
 
@@ -32,7 +32,7 @@ def regression_example_gqa_sink_fwd_bhsd_wgmma_pipelined():
 
 def regression_example_gqa_sink_fwd_bhsd_wgmma_pipelined_sliding_window():
     tilelang.testing.process_func(
-        example_gqa_sink_fwd_bhsd_wgmma_pipelined.run_regression_perf, window_size=128
+        example_gqa_sink_fwd_bhsd_wgmma_pipelined.run_regression_perf, "regression_example_gqa_sink_fwd_bhsd_wgmma_pipelined_sliding_window", window_size=128
     )
 
 
@@ -42,7 +42,7 @@ def regression_example_mha_sink_bwd_bhsd():
 
 def regression_example_mha_sink_bwd_bhsd_sliding_window():
     tilelang.testing.process_func(
-        example_mha_sink_bwd_bhsd.run_regression_perf, window_size=128
+        example_mha_sink_bwd_bhsd.run_regression_perf, "regression_example_mha_sink_bwd_bhsd_sliding_window", window_size=128
     )
 
 
@@ -52,7 +52,7 @@ def regression_example_gqa_sink_bwd_bhsd():
 
 def regression_example_gqa_sink_bwd_bhsd_sliding_window():
     tilelang.testing.process_func(
-        example_gqa_sink_bwd_bhsd.run_regression_perf, window_size=128
+        example_gqa_sink_bwd_bhsd.run_regression_perf, "regression_example_gqa_sink_bwd_bhsd_sliding_window", window_size=128
     )
 
 
