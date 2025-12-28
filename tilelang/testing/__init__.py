332b1721154e84f39e3ad7564d6e743f06d29f9f
xwhzz <wh.xie@outlook.com>
2025-12-28 02:41:47 +0000

    fix

--- tilelang/testing/__init__.py
+++ tilelang/testing/__init__.py
@@ -8,7 +8,7 @@ from tilelang.contrib import nvcc
 from tvm.testing.utils import requires_cuda, requires_package, requires_llvm, requires_metal, requires_rocm, _compose
 
 from tilelang.utils.tensor import torch_assert_close as torch_assert_close
-from .perf_regression import regression_all, process_func, regression
+from .perf_regression import process_func, regression
 
 __all__ = [
     "requires_package",
@@ -18,7 +18,6 @@ __all__ = [
     "requires_llvm",
     "main",
     "requires_cuda_compute_version",
-    "regression_all",
     "process_func",
     "regression",
 ] + [f"requires_cuda_compute_version_{op}" for op in ("ge", "gt", "le", "lt", "eq")]
